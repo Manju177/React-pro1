bcc744fa46a8abd30a46a066b1c0921c03be40b4
Manju177 pattanashetti <manjunathpattanashetti166@gmail.com>
2023-11-25 22:17:22 -0800

    changs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+import SiginUp from './Pages/SiginUp'
+import Profile from './Pages/Profile'
+import Header from './components/Header'
 
 function App() {
   return (
     <div>
-      <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/signin' element={<SignIn/>}/>
+      <Route path='/signup' element={<SiginUp/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
     </div>
+    
   )
 }
 
